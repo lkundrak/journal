cc89b1b215836d87e5bd437699f01627df6fd1e1
Lubomir Rintel <lkundrak@v3.sk>
2010-12-21 10:08:58 +0100

    Fix date parse

--- scripts/creationdate.sh
+++ scripts/creationdate.sh
@@ -9,7 +9,7 @@ shift
 for FILE in $*
 do
 	DATE="$(sed -n 's/<!-- date:\(.*\) -->/\1/p' $FILE)"
-	[ "$DATE" ] || DATE="$(git log $FILE | awk '/Date:/ {gsub ("Date: *", ""); gsub ("+.*",""); d=$0} END {print d}')"
+	[ "$DATE" ] || DATE="$(git log $FILE | awk '/Date:/ {gsub ("Date: *", ""); gsub ("[-+].*",""); d=$0} END {print d}')"
 	DATE="$(date "+$FORMAT" -d "$DATE")"
 	echo -n $DATE
 	echo "$(echo $* |grep -q ' ' && echo -e "\t$FILE")"
